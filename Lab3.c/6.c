3f23388fa3b15130ed55776013f844c73f210625
Poonam-MCA-SEM-1-Roll-34 <poonamchouhan1005@gmail.com>
2025-01-16 06:27:38 -0800

    6.c

---
+++ Lab3.c/6.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int deleteElement(int arr[], int n, int x)
+{
+	
+	int i;
+	for (i = 0; i < n; i++) {
+		if (arr[i] == x) {
+			break;
+		}
+	}
+
+	
+	if (i < n) {
+		n = n - 1;
+		for (int j = i; j < n; j++) {
+			arr[j] = arr[j + 1];
+		}
+	}
+
+	return n;
+}
+
+
+int main()
+{
+	int arr[] = { 11, 15, 6, 8, 9, 10 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int x = 6;
+
+	
+	n = deleteElement(arr, n, x);
+
+	printf("delete array is \n");
+	for (int i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
+
+	return 0;
+}
